c731c4da653b4746b20c8aa0a9171733b631792c
Hieu Mau <mauhieu12@gmail.com>
2023-06-22 21:40:28 +0700

    refactor

--- index.js
+++ index.js
@@ -1,48 +1,38 @@
-import { minimalSetup, EditorView } from "codemirror";
+import {minimalSetup, EditorView} from "codemirror";
 
-import { keymap } from "@codemirror/view";
+import {keymap} from "@codemirror/view";
 
-import { wolframLanguage } from "./src/mathematica/mathematica";
- 
-import { bracketMatching } from "@codemirror/language"
+import {wolframLanguage} from "./src/mathematica/mathematica";
+
+import {bracketMatching} from "@codemirror/language"
 
 import rainbowBrackets from 'rainbowbrackets'
 
 
 let editorCustomTheme = EditorView.theme({
-  "&.cm-focused": {
-    outline: "dotted 1px black",
-    background: 'inherit'
-  },
-  ".cm-line": {
-    padding: 0,
-    'padding-left': '2px',
-    'align-items': 'center'
-  },
-  ".cm-activeLine": {
-    'background-color': 'transparent'
-  }
+    "&.cm-focused": {
+        outline: "dotted 1px black", background: 'inherit'
+    }, ".cm-line": {
+        padding: 0, 'padding-left': '2px', 'align-items': 'center'
+    }, ".cm-activeLine": {
+        'background-color': 'transparent'
+    }
 });
 
 let doc = `NIntegrate((x-1)*(x-0.5)*x*(x+0.5)*(x+1), {x,0,1}, Method->LegendreGauss)`;
 
 
 let Editor = new EditorView({
-  doc: doc,
-  extensions: [
-    minimalSetup,
-    editorCustomTheme,   
-    wolframLanguage, 
-    bracketMatching(),
-    rainbowBrackets(),
-    keymap.of([
-      { key: "Shift-Enter", preventDefault: true, run: function (editor, key) { 
-        //here you can evaluate the result
-        alert(editor.state.doc.toString());
-        return true;
-      } }
-    ])
-  ],
-  parent: document.querySelector("#editor")
+    doc: doc,
+    extensions: [minimalSetup, editorCustomTheme, wolframLanguage, bracketMatching(), rainbowBrackets(), keymap.of([{
+        key: "Shift-Enter", preventDefault: true, run: function (editor, key) {
+            //here you can evaluate the result
+            alert(editor.state.doc.toString());
+            return true;
+        }
+    }]), EditorView.updateListener.of(function (e) {
+        console.log(e.state.doc.toString())
+    })],
+    parent: document.querySelector("#editor"),
 });
 
